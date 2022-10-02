7c97db4cf2f234a3fd6b65175bf4e52be47de7b0
Charlie Williams <c@harl.ie>
2022-10-02 09:26:46 +0100

    Initial Commit

---
+++ Rickface/RickfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickfaceApp.swift
+//  Rickface
+//
+//  Created by Charlie Williams on 02/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RickfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
