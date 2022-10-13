4ffa2b4b706b744370cfb0e93732e99d790c3c4d
Charlie Williams <c@harl.ie>
2022-10-13 13:51:55 +0100

    add target

---
+++ RickWatch Watch App/RickWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickWatchApp.swift
+//  RickWatch Watch App
+//
+//  Created by Charlie Williams on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RickWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
